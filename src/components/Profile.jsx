d9cb22c8af00835da833d6dbc5c1f28784964845
Snailoba <fameeverywhere@gmail.com>
2023-03-27 14:31:54 +0700

    Pollo

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import "./Profile.css";
+function Profile() {
+  return (
+    <div className="layout">
+      <img src="assets/betty.jpg" alt="profile" />
+      <h1 className="name">Betty White</h1>
+      <hr></hr>
+    </div>
+  );
+}
+export default Profile;
